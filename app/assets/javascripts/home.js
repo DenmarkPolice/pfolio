d53e208d868710c14941cf9ea677c0137b45ddd9
DenmarkPolice <23519801+DenmarkPolice@users.noreply.github.com>
2019-03-08 00:06:32 +0100

    Second project added attempt on bugfix

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -7,10 +7,10 @@ var tester = 0;
 var assignAllDiv = 0;
 var divs = [
       "På bar backe",
-      "Exempel 2",
-      "Annan sida",
-      "Fler sidor",
-      "Jag gjorde den"
+      "Portfolio",
+      "TBA",
+      "TBA",
+      "TBA"
     ];
 
 $(document).ready(function() {
@@ -61,6 +61,8 @@ $(document).ready(function() {
 
     $(function(){
         $('.scroll').on('click', function(){
+
+          $(".second-scroll-container").css({"display":"block"})
 
           setTimeout(function() {
               $(".intro").fadeOut(1500);
@@ -120,16 +122,17 @@ function displayHelper(text) {
 
     if (text == divs[tester]) {
 
-      menuDiv = ".sScroll" + tester;
-      $(menuDiv).css({
-        "pointer-events": "none",
-        "cursor": "default"//,
-        //"background-color": "#FFFFFF",
-        //"color": "#4f4f4f"
-      });
-
+      //menuDiv = ".sScroll" + tester;
+      //menuDivP = ".sScroll" + tester + " p";
+      //$(menuDiv).css({
+      //  "pointer-events": "none",
+      //  "cursor": "default",
+      //  "background-color": "#FFFFFF",
+      //  "color": "#4f4f4f"
+      //});
 
       targetDiv = ".sScontent" + tester;
+      $(targetDiv).css({"display": "block"});
       $(targetDiv).animate({"opacity": "1"}, 1500 );
 
       assignAllDiv = tester;
@@ -140,12 +143,16 @@ function displayHelper(text) {
           assignAllDiv = 0;
         }
 
-        menuDiv = ".sScroll" + assignAllDiv;
-        $(menuDiv).css({"pointer-events": "initial","cursor": "pointer"});
-        //$(menuDiv).css({"background-color": "#","color": "#FFFFFF"});
+        //menuDiv = ".sScroll" + assignAllDiv;
+        //menuDivP = ".sScroll" + assignAllDiv + " p";
+        //$(menuDiv).css({"pointer-events": "initial","cursor": "pointer"});
+        //$(menuDiv).css({"background-color": "transparent", "color": "#FFFFFF"});
+
+        //$(".second-scroll:hover").css({"background-color": "#FFFFFF", "color": "#4f4f4f"});
 
         targetDiv = ".sScontent" + assignAllDiv;
-        $(targetDiv).animate({"opacity": "0"}, 1500 );
+        $(targetDiv).animate({"opacity": "0"}, 500 );
+        $(targetDiv).css({"display": "none"});
 
         assignAllDiv += 1;
         assignAllDivCounter += 1;
@@ -158,4 +165,9 @@ function displayHelper(text) {
     }
     tester += 1;
   }
+}
+
+
+function openInNewTab(url) {
+  window.open(url);
 }
